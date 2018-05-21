48dc9b8112a4c6557db71ea4a34b4e7c36c2b2ad
jmoypace <jmoypace@gmail.com>
2018-05-21 11:33:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@
 def valid_move?(board, index)
 if(board[index]!="X" ||board[index]!="O")
   return true
-  elsif(board[index]-1=="X" ||board[index]-1=="O")
+  elsif(board[index-1]=="X" ||board[index-1]=="O")
   return false
   
 end
