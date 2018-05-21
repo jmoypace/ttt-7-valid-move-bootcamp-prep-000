bf17a390ae9a465226cfe1143f77027eb35c713d
jmoypace <jmoypace@gmail.com>
2018-05-21 11:45:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move?(board, index)
-if(board[index-1]>9 ||board[index-1]<0)
+if(board[index-1]!="" ||board[index-1]!=" ")
   return false
 
   
